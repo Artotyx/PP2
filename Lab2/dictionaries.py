c350fbdf7c6fba40120b0906e480fba4091a6661
Artotyx <138791387+Artotyx@users.noreply.github.com>
2024-02-10 14:33:29 +0600

    Add files via upload

---
+++ Lab2/dictionaries.py
@@ -0,0 +1,40 @@
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
+x=car.keys()
+print(x)
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["color"] = "red"
+x=car.keys()
+print(x)
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.pop("model")
+x=car.values()
+print(x)
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car.clear()
+x=car.items()
+print(x)
+
+
